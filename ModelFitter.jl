43ed0cce25eeae3a3de6c222c66d647bbe5830ce
Arnav Kumar <177550453+ArnavK04@users.noreply.github.com>
2026-07-27 10:32:25 +0000

    syncing

--- ModelFitter.jl
+++ ModelFitter.jl
@@ -243,14 +243,15 @@ function main()
     param_ref = Dict(p.key => p.refer for p in model.parameters)
 
     prior_kappa, gridx, gridy = LikelihoodFunctions.construct_prior(model; prior_flag=1, prior_value=p_value)
+    resolution = gridx[2,1] - gridx[1,1]
     # get full_kernel for the run
     full_kernel = FreeFormLens.compute_fullkernel(model, gridx, gridy)
 
     println("size of gridx: ", size(gridx))
     new_guess, _, _ = LikelihoodFunctions.construct_prior(model; prior_flag=g_flag, seed=seed,pix=pix,sigma=sigma, prior_value=guess_value)
 
-    filename = "MEM_fit_result4res_125FOV_reg$(reg_factor)_gflag$(g_flag)_pvalue$(p_value)_$(guess_value)_$(seed)_$(pix)_$(sigma)_$(runnumber)"
-    filename_fromprev = "MEM_fit_result4res_125FOV_reg$(reg_factor)_gflag$(g_flag)_pvalue$(p_value)_$(guess_value)_$(seed)_$(pix)_$(sigma)_$(runnumber+1)"
+    filename = "MEM_fit_result4res_125FOV_reg$(reg_factor)_gflag$(g_flag)_pvalue$(p_value)_$(guess_value)_$(seed)_$(pix)_$(sigma)_$(runnumber)_$(resolution)"
+    filename_fromprev = "MEM_fit_result4res_125FOV_reg$(reg_factor)_gflag$(g_flag)_pvalue$(p_value)_$(guess_value)_$(seed)_$(pix)_$(sigma)_$(runnumber+1)_$(resolution)"
 
     # load prior from previous run converged map and refine to a finer grid
     if prior_from_prev
