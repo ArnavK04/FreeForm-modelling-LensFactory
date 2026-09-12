06c11dc473b90e9096b4dd0cfb25458d92aff060
Arnav Kumar <177550453+ArnavK04@users.noreply.github.com>
2026-09-12 19:02:14 +0000

    added self reiterations to check

--- ModelFitter.jl
+++ ModelFitter.jl
@@ -432,16 +432,26 @@ function main()
     end
 
     saved_file, used_runnumber, used_file = run_optimizer(seed, input_file, pix, sigma, g_flag, inc_res, same_res, guess_value, runnumber, p_value, clustername, prevfile)  # first run
+    println(saved_file, "--------------", used_runnumber, "-------------", used_file)
     saved_file2, used_runnumber2, used_file2 = run_optimizer(seed, input_file, pix, sigma, g_flag, false, true, guess_value, used_runnumber, p_value, clustername, saved_file)  # second run
+    println(saved_file2, "--------------", used_runnumber, "-------------", used_file2)
     saved_file3, used_runnumber3, used_file3 = run_optimizer(seed, input_file, pix, sigma, g_flag, false, true, guess_value, used_runnumber2 + 1, p_value, clustername, saved_file2)  # third run
+    println(saved_file3, "--------------", used_runnumber3, "-------------", used_file3)
     saved_file4, used_runnumber4, used_file4 = run_optimizer(seed, input_file, pix, sigma, g_flag, false, true, guess_value, used_runnumber3 + 1, p_value, clustername, saved_file3)  # fourth run
+    println(saved_file4, "--------------", used_runnumber4, "-------------", used_file4)
     saved_file5, used_runnumber5, used_file5 = run_optimizer(seed, input_file, pix, sigma, g_flag, false, true, guess_value, used_runnumber4 + 1, p_value, clustername, saved_file4)  # fifth run
+    println(saved_file5, "--------------", used_runnumber5, "-------------", used_file5)
 
     saved_file6, used_runnumber6, used_file6 = run_optimizer(seed, input_file, pix, sigma, g_flag, true, false, guess_value, used_runnumber5 + 1, p_value, clustername, saved_file5)  # sixth run higher res
+    prinltln(saved_file6, "--------------", used_runnumber6, "-------------", used_file6)
     saved_file7, used_runnumber7, used_file7 = run_optimizer(seed, input_file, pix, sigma, g_flag, false, true, guess_value, used_runnumber6 + 1, p_value, clustername, saved_file6)  # seventh run higher res
+    println(saved_file7, "--------------", used_runnumber7, "-------------", used_file7)
     saved_file8, used_runnumber8, used_file8 = run_optimizer(seed, input_file, pix, sigma, g_flag, false, true, guess_value, used_runnumber7 + 1, p_value, clustername, saved_file7)  # eighth run higher res
+    println(saved_file8, "--------------", used_runnumber8, "-------------", used_file8)
     saved_file9, used_runnumber9, used_file9 = run_optimizer(seed, input_file, pix, sigma, g_flag, false, true, guess_value, used_runnumber8 + 1, p_value, clustername, saved_file8)  # ninth run higher res
+    println(saved_file9, "--------------", used_runnumber9, "-------------", used_file9)
     saved_file9, used_runnumber10, used_file10 = run_optimizer(seed, input_file, pix, sigma, g_flag, false, true, guess_value, used_runnumber9 + 1, p_value, clustername, saved_file9)  # tenth run higher res
+    println(saved_file9, "--------------", used_runnumber10, "-------------", used_file10)
 
 
 end
